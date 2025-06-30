ea2d964a58ea772ebe6432db56f5110db657274a
Bunjiro Yamada <bunjiroyamada@Takihyos-MacBook-Pro.local>
2025-06-30 16:14:23 -0400

    css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import Homepage from './components/homepage.jsx'
 import './App.css'
 
 function App() {
-  return <Homepage />
+  const [count, setCount] = useState(0)
+
+  return (
+      return <Homepage />
+  )
 }
 
 export default App
