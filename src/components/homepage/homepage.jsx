3f74f3ca06ec00c7bd6bb0743680d1419f028ceb
Bunjiro Yamada <bunjiroyamada@Takihyos-MacBook-Pro.local>
2025-08-17 16:21:36 -0400

    middle of fixing layout

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -1,87 +1,58 @@
 import React from "react";
-import styles from './homepage.module.css'
+import styles from "./Homepage.module.css";
 
 const Homepage = () => {
   return (
-    <div className={styles.homepage} data-model-id="1:2">
-      <div className={styles.div}>
-        <div className={styles.overlap}>
-          <div className={styles.rectangle} />
+    <div className={styles.container}>
+      <div className={styles.buncard + ' ' + styles.nameCard}>
+        <h1>
+          <span className={styles.blackText}>Bunjiro</span>
+          <br />
+          <span className={styles.grayText}>Yamada</span>
+        </h1>
+      </div>
 
-          <p className={styles["bunjiro-yamada"]}>
-            <span className={styles["text-wrapper"]}>
-              Bunjiro <br />
-            </span>
-
-            <span className={styles.span}>Yamada</span>
-          </p>
-        </div>
-
+      <div className={styles.card + ' ' + styles.photoCard}>
         <img
-          className={styles.img}
-          alt="Rectangle"
-          src="https://c.animaapp.com/mcjh3hguLTu7SD/img/rectangle-4.png"
+          src="bunjiro-photo.jpg"
+          alt="Bunjiro"
+          className={styles.circleImage}
         />
+      </div>
 
-        <div className={styles["overlap-group"]}>
-          <p className={styles["see-my-latest-work"]}>
-            <span className={styles["text-wrapper-2"]}>See my latest </span>
+      <div className={styles.card + ' ' + styles.bioCard}>
+        <p>
+          <strong>Lorem ipsum dolor sit amet,</strong> consectetur adipiscing elit.
+          Vestibulum odio arcu, efficitur nec risus ut, porta auctor lorem.
+          <strong> Phasellus rutrum quam sed tincidunt varius.</strong> Suspendisse.
+        </p>
+      </div>
 
-            <span className={styles["text-wrapper-3"]}>work</span>
-          </p>
+      <div className={styles.card + ' ' + styles.imageCard}>
+        <img src="building.jpg" alt="building" className={styles.roundedImage} />
+      </div>
 
-          <div className={styles["overlap-2"]}>
-            <img
-              className={styles.arrow}
-              alt="Arrow"
-              src="https://c.animaapp.com/mcjh3hguLTu7SD/img/arrow-11.svg"
-            />
-
-            <div className={styles["rectangle-2"]} />
-          </div>
-
-          <div className={styles["text-wrapper-4"]}>All Projects</div>
+      <div className={styles.card + ' ' + styles.projectCard}>
+        <h2>
+          <span className={styles.blackText}>See my latest</span>
+          <br />
+          <span className={styles.grayText}>work</span>
+        </h2>
+        <div className={styles.projectFooter}>
+          <span className={styles.allProjects}>All Projects</span>
+          <span className={styles.arrow}>&rarr;</span>
         </div>
+      </div>
 
-        <div className={styles["div-wrapper"]}>
-          <p className={styles.p}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-            odio arcu, efficitur nec risus ut, porta auctor lorem. Phasellus
-            rutrum quam sed tincidunt varius. Suspendisse.
-          </p>
-        </div>
-
-        <div className={styles["ellipse-wrapper"]}>
-          <img
-            className={styles.ellipse}
-            alt="Ellipse"
-            src="https://c.animaapp.com/mcjh3hguLTu7SD/img/ellipse-1.png"
-          />
-        </div>
-
-        <div className={styles["overlap-group-2"]}>
-          <div className={styles["rectangle-3"]} />
-
-          <p className={styles["contact-me"]}>
-            <span className={styles["text-wrapper"]}>
-              Contact
-              <br />
-            </span>
-
-            <span className={styles.span}>me</span>
-          </p>
-
-          <img
-            className={styles.image}
-            alt="Image"
-            src="https://c.animaapp.com/mcjh3hguLTu7SD/img/image-1.png"
-          />
-
-          <img
-            className={styles["image-2"]}
-            alt="Image"
-            src="https://c.animaapp.com/mcjh3hguLTu7SD/img/image-2.png"
-          />
+      <div className={styles.card + ' ' + styles.contactCard}>
+        <h2>
+          <span className={styles.blackText}>Contact</span>
+          <br />
+          <span className={styles.grayText}>me</span>
+        </h2>
+        <div className={styles.iconRow}>
+          <img src="github-icon.png" alt="GitHub" className={styles.icon} />
+          <img src="linkedin-icon.png" alt="LinkedIn" className={styles.icon} />
         </div>
       </div>
     </div>
